6873ed9a2fa5512a766d83e8f6bc26df4f8cb293
Rudy <rudyax2004@gmail.com>
2025-08-06 19:27:36 -0600

    tercer Commit

--- Actividad12Progra.py
+++ Actividad12Progra.py
@@ -1,12 +1,26 @@
 repartidores = {}
 opcion = 0
 
+def quick_sort(resultado):
+    if len(resultado) <= 1:
+        return resultado
+
+    pivote = resultado[0]
+    menores = [x for x in resultado[1:] if x < pivote]
+    iguales = [x for x in resultado if x == pivote]
+    mayores = [x for x in resultado[1:] if x > pivote]
+
+    return quick_sort(menores) + iguales + quick_sort(mayores)
+
+
+
 print('Bienvenidos Actividad 12')
 while opcion != 3:
     print('Bienvenido')
     print("1.- Registrar")
     print("2.- Mostrar")
     print("3.- Salir")
+    opcion = int(input("Ingrese la opcion que desea: "))
 
     match opcion:
         case 1:
@@ -18,6 +32,11 @@ while opcion != 3:
                 repartidores[nombre]["Paquetes"] = int(input("Ingrese la cantidad de paquetes entregados"))
                 repartidores[nombre]["Zona"] = input("Ingrese la zona donde fue asignad@")
 
+
+        case 2:
+            lista = list(repartidores.items())
+            resultado = quick_sort(lista)
+            print(resultado)
 
 
 
